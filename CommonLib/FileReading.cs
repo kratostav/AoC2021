c6c5f31d1b4730161fc826573f167b806d02a127
André Schretter <andre.schretter@novuss-automation.com>
2021-12-01 17:41:21 +0100

    added Day1 Solution

---
+++ CommonLib/FileReading.cs
@@ -0,0 +1,36 @@
+﻿namespace CommonLib
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class FileReading
+    {
+        public static IEnumerable<string> GetInput(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                return File.ReadAllLines(filename).ToList();
+            }
+            throw new Exception($"Couldn't locate input file {filename}");
+        }
+
+        public static IEnumerable<int> AllNumbers(IEnumerable<string> input)
+        {
+            var integers = input.Select(int.Parse);
+
+            return integers;
+        }
+
+        public static IEnumerable<int> AllNumbers(string filename)
+        {
+            var integers = AllNumbers(GetInput(filename));
+
+            return integers;
+        }
+
+    }
+}
--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,12 +1,49 @@
 ﻿namespace Day01
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using CommonLib;
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var input = FileReading.AllNumbers("input.txt"); // Read the Input file
+            Console.WriteLine("AoC - Day01");
+            Console.WriteLine("Solution Part 1:");
+            Console.WriteLine(CountIncreasingSums(input, 1));
+            Console.WriteLine("Solution Part 2:");
+            Console.WriteLine(CountIncreasingSums(input, 3));
+            Console.ReadLine();
+        }
+
+        static int CountIncreasingSums(IEnumerable<int> numbers, int windowSize)
+        {
+            var copy = numbers;
+
+            var rounds = numbers.Count() - windowSize; //calculate the Rounds via the Window size (needed for the Loop)
+            var count = 0;
+
+            for (var i = 0; i < rounds; i++)
+            {
+                // Split the List (left and right will be compared against)
+                var left = copy;
+                var right = copy.Skip(1);
+                // sum up by the windows size
+                var leftSum = left.Take(windowSize).Sum();
+                var rightSum = right.Take(windowSize).Sum();
+                // increase the count when the right side is bigger
+                if (leftSum < rightSum)
+                {
+                    ++count;
+                }
+                // advance the List
+                copy = copy.Skip(1);
+            }
+
+            return count;
         }
     }
 }
